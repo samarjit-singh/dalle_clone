e7c0a7e273d6d215df995b6f02ca2ace6ba23a6e
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2023-02-02 22:28:15 +0530

    completed home page and started working on createPost

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+  const handleChange = (e) => {};
+  const handleSurpriseMe = () => {};
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+          Generate an imaginative image through DALL-E AI and share it with the
+          community
+        </p>
+      </div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            lableName="Your Name"
+            type="text"
+            name="name"
+            placeholder="samar"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            lableName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A plush toy robot sitting against a yellow wall"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
---
+++ client/src/utils/index.js
@@ -0,0 +1,11 @@
+import FileSaver from "file-saver";
+import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const RandomPrompt = surpriseMePrompts[randomIndex];
+
+  if (RandomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return RandomPrompt;
+}
