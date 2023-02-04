02f923efa6932a15be0453b880e408af5db2f902
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2023-02-04 22:26:53 +0530

    connected with mongodb and started making api calls to generate AI images

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
-// import connectDB from "./mongodb/connect.js";
-// import postRoutes from "./routes/postRoutes.js";
-// import dalleRoutes from "./routes/dalleRoutes.js";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
@@ -12,14 +12,23 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
+
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E!");
 });
 
 const startServer = async () => {
-  app.listen(8080, () =>
-    console.log("Server has started on port https://localhost:8080")
-  );
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port https://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
