3277b356ea485dfb6664ef9c0a2465222bdb0272
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2023-02-05 16:41:58 +0530

    updated url

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://dall-e-4e9j.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
-
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E!");
 });
